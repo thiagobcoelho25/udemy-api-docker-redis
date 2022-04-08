e4e9a0eaf7de356234fa0bd01740d9a7b2f00a96
Thiago <thiagobcoelho25@gmail.com>
2022-04-08 19:30:16 -0300

    configuração migrations no Typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-import { json } from 'stream/consumers';
+import '@shared/typeorm';
 
 const app = express();
 
