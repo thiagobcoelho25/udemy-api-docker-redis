8cc61d9167971e2096101dcfe7f57f31c8407414
Thiago <thiagobcoelho25@gmail.com>
2022-04-22 10:11:36 -0300

    Camada de Infra - Refactoring TypeORM Repositories

--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -1,26 +1,41 @@
+import { ICreateCustomer } from '@modules/customers/domain/models/ICreateCustomer';
 import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import Customer from '../entities/Customer';
 
-@EntityRepository(Customer)
-class CustomersRepository
-  extends Repository<Customer>
-  implements ICustomersRepository
-{
+class CustomersRepository implements ICustomersRepository {
+  private ormRepository: Repository<Customer>;
+
+  constructor() {
+    this.ormRepository = getRepository(Customer);
+  }
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = this.ormRepository.create({ name, email });
+
+    await this.ormRepository.save(customer);
+
+    return customer;
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    return await this.ormRepository.save(customer);
+  }
+
   public async findByName(name: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({ where: { name } });
+    const customer = await this.ormRepository.findOne({ where: { name } });
 
     return customer;
   }
 
   public async findById(id: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({ where: { id } });
+    const customer = await this.ormRepository.findOne({ where: { id } });
 
     return customer;
   }
 
   public async findByEmail(email: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({ where: { email } });
+    const customer = await this.ormRepository.findOne({ where: { email } });
 
     return customer;
   }
